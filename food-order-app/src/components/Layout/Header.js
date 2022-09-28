fd410b9acf9e4f25d0c44f33add2302ed7944907
Harry Dhillon <hdhillon478@gmail.com>
2022-09-28 13:37:59 +0100

    Add jsx boilerplated to Header component

--- food-order-app/src/components/Layout/Header.js
+++ food-order-app/src/components/Layout/Header.js
@@ -1,7 +1,17 @@
-import React from "react";
+import classes from "./Header.module.css";
 
-const Header = () => {
-  return <div>Header</div>;
+const Header = (props) => {
+  return (
+    <>
+      <header>
+        <h1>React Meals</h1>
+        <button>Cart</button>
+      </header>
+      <div>
+        <img src='' alt='' />
+      </div>
+    </>
+  );
 };
 
 export default Header;
