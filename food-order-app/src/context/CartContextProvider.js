424aa9cfcb1edc47a3f1d39dc49d2c73e3d4c2a6
Harry Dhillon <hdhillon478@gmail.com>
2022-09-29 15:47:14 +0100

    Add a reducer hook to handle adding items to cart

--- food-order-app/src/context/CartContextProvider.js
+++ food-order-app/src/context/CartContextProvider.js
@@ -1,22 +1,55 @@
 import React from "react";
+import { useReducer } from "react";
 
 import CartContext from "./cart-context";
+
+// default cart state
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+// cart reducer function
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    // rather than modifying the old array, concat returns a new array
+    const updatedItems = state.items.concat(action.item);
+    // calculate the total value of cart items
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  return defaultCartState;
+};
 
 /*
   Component to handle the provision
   of Cart data using the context api
 */
 const CartContextProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
   // add an item to the cart
-  const addItemToCartHandler = (item) => {};
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({
+      type: "ADD_ITEM",
+      item: item,
+    });
+  };
 
   // remove an item from the cart
   const removeItemFromCartHandler = (id) => {};
 
   // cart content which will be provided and updated
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
