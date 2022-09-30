91673ba345421651ddc39f1ba07be11308274735
Harry Dhillon <hdhillon478@gmail.com>
2022-09-30 21:38:03 +0100

    Add useRef hook and validation check

--- food-order-app/src/components/Meals/MealItem/MealItemForm.js
+++ food-order-app/src/components/Meals/MealItem/MealItemForm.js
@@ -1,14 +1,31 @@
+import { useRef } from "react";
 import Input from "./Input";
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const amountInputRef = useRef();
+
   const submitHandler = (event) => {
     event.preventDefault();
+
+    // ref values are always returned as string and need to converted to integer
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    // validation check
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      return;
+    }
   };
 
   return (
     <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label='Amount'
         input={{
           id: "amount",
