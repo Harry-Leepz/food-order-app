4dbf316138f0b50d49d992912257d8b1ebb851e1
Harry Dhillon <hdhillon478@gmail.com>
2022-10-01 15:38:50 +0100

    Show Cart total amount, instead of hardcoded dummy value

--- food-order-app/src/components/Cart/Cart.js
+++ food-order-app/src/components/Cart/Cart.js
@@ -6,6 +6,8 @@ import classes from "./Cart.module.css";
 const Cart = (props) => {
   const cartContext = useContext(CartContext);
 
+  const totalAmount = cartContext.totalAmount.toFixed(2);
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartContext.items.map((item) => (
@@ -19,7 +21,7 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>${totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onHideCart}>
