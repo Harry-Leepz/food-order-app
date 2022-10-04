805a5aaeba7aa7432782dbbd85bfb3bb085ae36e
Harry Dhillon <hdhillon478@gmail.com>
2022-10-04 13:13:11 +0100

    Add logic to add and remove the animation to button element

--- food-order-app/src/components/Layout/HeaderCartButton.js
+++ food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,37 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../context/cart-context";
-
 import CartIcon from "../Cart/CartIcon";
 
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [buttonAnimation, setButtonAnimation] = useState(false);
   const cartContext = useContext(CartContext);
+  const { items } = cartContext;
 
-  const numberOfCartItems = cartContext.items.reduce((currentNumber, item) => {
+  const numberOfCartItems = items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
 
-  const buttonClasses = `${classes.button} ${classes.bump}`;
+  const buttonClasses = `${classes.button} ${
+    buttonAnimation ? classes.bump : ""
+  }`;
+
+  // Apply css animation (bump) to the button element
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setButtonAnimation(true);
+
+    const timer = setTimeout(() => {
+      setButtonAnimation(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
 
   return (
     <button onClick={props.onClick} className={buttonClasses}>
