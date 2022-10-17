a6ecb5f8cdbb906201f7707546988f37ffd6bc14
Harry Dhillon <hdhillon478@gmail.com>
2022-10-17 12:50:49 +0100

    Add CSS classes for validation messages

--- food-order-app/src/components/Cart/Checkout.js
+++ food-order-app/src/components/Cart/Checkout.js
@@ -49,26 +49,42 @@ const Checkout = (props) => {
 
   return (
     <form className={classes.form} onSubmit={onSubmitHandler}>
-      <div className={classes.control}>
+      <div
+        className={`${classes.control} ${
+          formInputsValidity.name ? "" : classes.invalid
+        }`}
+      >
         <label htmlFor='name'>Your Name</label>
         <input type='text' id='name' ref={nameInput} />
         {!formInputsValidity.name && <small> Please enter your name!</small>}
       </div>
-      <div className={classes.control}>
+      <div
+        className={`${classes.control} ${
+          formInputsValidity.street ? "" : classes.invalid
+        }`}
+      >
         <label htmlFor='street'>Street</label>
         <input type='text' id='street' ref={streetInput} />
         {!formInputsValidity.street && (
           <small> Please enter your street address!</small>
         )}
       </div>
-      <div className={classes.control}>
+      <div
+        className={`${classes.control} ${
+          formInputsValidity.postal ? "" : classes.invalid
+        }`}
+      >
         <label htmlFor='postal'>Postal Code</label>
         <input type='text' id='postal' ref={postalInput} />
         {!formInputsValidity.postal && (
           <small> Please enter your postal code!</small>
         )}
       </div>
-      <div className={classes.control}>
+      <div
+        className={`${classes.control} ${
+          formInputsValidity.city ? "" : classes.invalid
+        }`}
+      >
         <label htmlFor='city'>City</label>
         <input type='text' id='city' ref={cityInput} />
         {!formInputsValidity.city && <small> Please enter your city!</small>}
